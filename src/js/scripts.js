c18c58737efb033b412d98f353e149d789d1b2c1
gabrielsilvacodes <gabrielsilvadeveloper@outlook.com>
2025-04-11 01:51:27 -0300

    js iniciado

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,34 @@
-// 43d4bdbec6fae6c88d6606981cfe84d3
+//Variáveis e selção de elementos
+const apiKey = "43d4bdbec6fae6c88d6606981cfe84d3";
+const apiCountryURl = "";
+
+const cityInput = document.querySelector("#city-input");
+const aearchBtn = document.querySelector("search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const iconElement = document.querySelector("#temp_img");
+
+const countryElement = document.querySelector("#country");
+const tempMaxElement = document.querySelector("#temp_max");
+const tempMinElement = document.querySelector("#temp_min");
+const humidityElement = document.querySelector("#humidity");
+const windElement = document.querySelector("#wind");
+
+const weatherContainer = document.querySelector("weather-data");
+
+//Funções
+const getWeatherData = async (city) => {
+  const apiWeatherURL =
+    "https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=${apikey}&lang=pt_br";
+};
+
+const showWeatherData = (city) => {};
+
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+});
